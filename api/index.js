6d88358c7eaf7e757cd2b4b5dc477f738fddc56d
Mrigyan <mrigyanmaharjan@gmail.com>
2024-04-13 07:28:21 +0545

    items

--- api/index.js
+++ api/index.js
@@ -22,11 +22,6 @@ const PORT=process.env.PORT
 app.use(cors());
 app.use(express.json())
 
-app.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); 
-   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-   next();
- });
 
 app.listen(PORT,(req,res)=>{
    console.log(`server running at port ${PORT}`)
